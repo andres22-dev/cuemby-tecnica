524d6e61be7d463dca93b086e827c08e91765f1d
andres22 <afpinchao8@misena.edu.co>
2021-06-24 13:05:48 -0500

    conexion a base de datos completado y probando traer datos quemados a  la base de datos

--- app.js
+++ app.js
@@ -4,7 +4,33 @@ const url = 'mongodb://localhost/cuemby';
 
 mongoose.connect(url,{
 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
 
 })
-.then( ()=> console.log('CONEXION VALIDA :D HAPPY CODING!!') );
-.catch( ())
+.then( () => console.log('CONEXION VALIDA :D HAPPY CODING!!') )
+.catch( (e) => console.log('El error de conexion es : '+e))
+
+//Creando schema
+
+
+const jugadorSchema = mongoose.Schema({
+  noombre: String,
+  posicion: String,
+  nacionalidad: String,
+  equipo: String
+})
+
+const JugadorModel = mongoose.model('jugadores', jugadoresSchema)
+
+// 
+
+const mostrar = async () =>  {
+
+  const jugadores = await PersonaModel.find()
+  console.log(personas)
+}
+
+mostrar();
