e3891b463a78e964dc0d8d8e9a7afb8e1e646844
andres22 <afpinchao8@misena.edu.co>
2021-06-24 15:10:34 -0500

    terminando funcion que trae datos de la peticion jugadores aleatoriamente y los guarda en un array

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-
+const axios = require('axios');
 const url = 'mongodb://localhost/cuemby';
+const urlApi = "https://www.easports.com/fifa/ultimate-team/api/fut/item";
 
 mongoose.connect(url,{
 
@@ -28,6 +29,30 @@ const JugadorModel = mongoose.model('jugadores', jugadorSchema)
 
 // trayendo datos por peticiones de la API 
 
+const maximun = 1;
+const minimun = 20;
+const random = () => Math.floor(Math.random()*(maximun - minimun)+ minimun);
+
+  async function getJugadores() {
+  try {
+    const response = await axios.get(urlApi);
+
+    const jugadoresArray = [];
+
+    for (let i = 0; i < 4; i++){
+
+      let jugadorNombre = await response.data.items[random()].name;
+      let jugadorPosicion = await response.data.items[random()].position;
+      let jugadorNacionalidad = await response.data.items[random()].nation.name;
+      let jugadorEquipo = await response.data.items[random()].club.name;
+      jugadoresArray.push([jugadorNombre, jugadorPosicion, jugadorNacionalidad, jugadorEquipo]);
+    }
+    console.log(jugadoresArray);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 
 // trayendo datos quemados de la bd
 
@@ -35,6 +60,7 @@ const mostrar = async () =>  {
 
   const jugadores = await JugadorModel.find()
   console.log(jugadores)
+  getJugadores()
 }
 
 mostrar();
